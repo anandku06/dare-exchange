c1cd5eba3df428137b07e3b121dd17f357ec9f40
Commiter <161126335+anandku06@users.noreply.github.com>
2025-06-29 02:16:07 +0530

    modified URLs for smooth routing and templates for inherited components

--- dareProject/dareProject/views.py
+++ dareProject/dareProject/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import (render)
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def feature(request):
+    return render(request, 'feature.html')
