d0ede132ecd2a4fe37e16fb4760237606083b3c8
Commiter <161126335+anandku06@users.noreply.github.com>
2025-07-21 21:43:45 +0530

    Implement user registration and login functionality, add dashboard app structure, and update settings for new app integration

--- dareProject/accounts/views.py
+++ dareProject/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def register(req):
@@ -7,6 +8,9 @@ def register(req):
         username = req.POST.get("username")
         email = req.POST.get("email")
         password = req.POST.get("password")
+
+        if User.objects.get(username).exists():
+            pass
 
         new_user = User.objects.create(
             username = username,
@@ -16,7 +20,7 @@ def register(req):
         new_user.set_password(password)
 
         new_user.save()
-
+        login(req, new_user)
         return redirect('home')
     return render(req, 'accounts/register.html')
 
@@ -25,10 +29,8 @@ def login_view(req):
         username = req.POST.get("username")
         password = req.POST.get("password")
 
-        pass
+
 
 def logout_view(req):
-    if req.method == "POST":
-        # Assuming you have a logout function in your views
-        return redirect('home')
-    return render(req, 'accounts/logout.html')
+    logout(req)
+    return redirect('home')
